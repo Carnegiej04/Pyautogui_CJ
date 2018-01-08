2588d5f5bfdc48041857848e30edf0e0c6d3e85f
Carnegiej04 <33633101+Carnegiej04@users.noreply.github.com>
2018-01-08 11:34:07 -0500

    Add files via upload

---
+++ pg_CJ.py
@@ -0,0 +1,12 @@
+import pyautogui as pg
+
+pg.click(1107, 35, 3)
+pg.typewrite('espn.com\n', 0.5)
+pg.PAUSE = 
+pg.click(1313, 129, 1)
+pg.PAUSE = 7
+pg.click(1318, 179, 1)
+pg.PAUSE = 7
+pg.click(1020, 217, 1)
+pg.PAUSE = 7
+pg.click(814, 534, 1)
